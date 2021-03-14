0a90b5df6778a03ec55f0d858c69884d548cbead
Rohan Khare <rnkhare@gmail.com>
2021-03-14 13:00:11 +0530

    Final Java code updated

--- CircleTest.java
+++ CircleTest.java
@@ -2,11 +2,13 @@ package io.junit5test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 class CircleTest {
 
 	@Test
+@RepeatedTest(4)
 	void testarea() {
 		CircleArea arecal= new CircleArea();
 		assertEquals(314.1592653589793, arecal.area(10),"This Method Calculates area of circle");
